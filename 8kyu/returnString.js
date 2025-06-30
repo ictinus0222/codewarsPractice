ba2f94298a8dfeaec6ab4ef5963235fcf88af9f4
Akhil Sharma <143958865+ictinus0222@users.noreply.github.com>
2025-06-30 19:01:38 +0530

    Solved returnString.js

---
+++ 8kyu/returnString.js
@@ -0,0 +1,10 @@
+/*Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+//My Solution
+
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`
+}
